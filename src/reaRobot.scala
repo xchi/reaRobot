40d4d54c32bdb39e157d3ff1cfbfbdf970ecaa6e
vivian.chi <vivian.chi@rea-group.com>
2017-03-30 17:27:03 +1100

    update

--- src/reaRobot.scala
+++ src/reaRobot.scala
@@ -10,8 +10,14 @@ object Turn extends Enumeration {
 }
 
 
-class robot(coordinateX:Int, coordinateY:Int, direction:Direction.Value){
+class robot(var coordinateX:Int, var coordinateY:Int, var direction:Direction.Value){
   def move():Unit={
+    def directionMatch(d:Direction.Value)=d match{
+      case Direction.North=>coordinateY=coordinateY+1
+      case Direction.East=>coordinateY=coordinateY+1
+      case Direction.South=>coordinateY=coordinateY+1
+      case Direction.West=>coordinateY=coordinateY+1
+    }
 
   }
 
